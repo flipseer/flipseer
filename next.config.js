d6a1996dd985af62dcb93349568f90382d1aa94d
flipseer <contact@flipseer.com>
2026-05-22 19:27:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
+const { withSentryConfig } = require('@sentry/nextjs')
+
 const nextConfig = {}
-module.exports = nextConfig
+
+module.exports = withSentryConfig(nextConfig, {
+  org: 'flipseer',
+  project: 'flipseer',
+  silent: true,
+  widenClientFileUpload: true,
+  hideSourceMaps: true,
+  disableLogger: true,
+})
