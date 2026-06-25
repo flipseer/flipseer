94b6df72488c755ab7828adfeef1316556d81215
flipseer <contact@flipseer.com>
2026-06-25 15:37:43 +0530

    Update page.tsx

--- app/groups/join/[code]/page.tsx
+++ app/groups/join/[code]/page.tsx
@@ -1,1 +1,8 @@
+import { redirect } from 'next/navigation';
 
+type Props = { params: { code: string } };
+
+export default function JoinRedirect({ params }: Props) {
+  const code = params.code?.toUpperCase() || '';
+  redirect(`/groups?join=${code}`);
+}
