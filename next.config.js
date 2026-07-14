a580e1db2910d2e21ebd86a7c33fe70a04096cec
flipseer <contact@flipseer.com>
2026-07-14 21:15:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -24,22 +24,6 @@ const nextConfig = {
             value: 'camera=(), microphone=(), geolocation=(self), interest-cohort=()',
           },
           {
-            key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://connect.facebook.net https://analytics.ahrefs.com https://static.clarity.ms",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "font-src 'self' https://fonts.gstatic.com",
-              "img-src 'self' data: blob: https: http:",
-              "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://api-sports.io https://v3.football.api-sports.io https://www.google-analytics.com https://analytics.google.com https://region1.google-analytics.com https://*.posthog.com https://*.sentry.io",
-              "frame-src 'none'",
-              "object-src 'none'",
-              "base-uri 'self'",
-              "form-action 'self'",
-              "upgrade-insecure-requests",
-            ].join('; '),
-          },
-          {
             key: 'Strict-Transport-Security',
             value: 'max-age=63072000; includeSubDomains; preload',
           },
