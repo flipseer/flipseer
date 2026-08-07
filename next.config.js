07ca6b8e9f58e4bc785bc332a16ba3e15d35ba51
flipseer <contact@flipseer.com>
2026-08-07 07:02:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const { withSentryConfig } = require('@sentry/nextjs')
-
 const nextConfig = {
   async headers() {
     return [
@@ -23,10 +22,24 @@ const nextConfig = {
         destination: '/nations',
         permanent: true,
       },
+      {
+        source: '/how-to-predict-football',
+        destination: '/guide/how-to-predict-football',
+        permanent: true,
+      },
+      {
+        source: '/football-reputation',
+        destination: '/guide/football-reputation',
+        permanent: true,
+      },
+      {
+        source: '/how-to-play',
+        destination: '/guide/how-to-play',
+        permanent: true,
+      },
     ]
   },
 }
-
 module.exports = withSentryConfig(nextConfig, {
   org: 'flipseer',
   project: 'flipseer',
